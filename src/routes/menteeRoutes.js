98690f6dcd4c24a86e986c9729ee988760e97061
Shirlene Teles <shiteles@gmail.com>
2022-12-13 17:27:06 -0300

    :safety_vest: Add authentication to routes

--- src/routes/menteeRoutes.js
+++ src/routes/menteeRoutes.js
@@ -3,11 +3,12 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/all", menteeController.findAllMentee );
-router.get("/:id", menteeController.findMenteeById);
+router.get("/:id", menteeController.menteeAuthentication, menteeController.findMenteeById);
 router.get("/", menteeController.findMenteeByName);
 router.post("/add", menteeController.createNewMentee)
-router.patch("/:id",  menteeController.updatementeeById);
-router.delete("/delete/:id", menteeController.deleteMenteeById)
 router.post("/login", menteeController.loginMentee);
+router.patch("/:id", menteeController.menteeAuthentication, menteeController.updatementeeById);
+router.delete("/delete/:id", menteeController.menteeAuthentication, menteeController.deleteMenteeById)
+
 
 module.exports = router
--- src/routes/mentorRoutes.js
+++ src/routes/mentorRoutes.js
@@ -4,11 +4,12 @@ const { menteeAuthentication } = require("../controllers/menteeController");
 const router = express.Router();
 
 router.get("/all", mentorController.findAllMentors);
-router.get("/:id", mentorController.findMentorById);
+router.get("/:id", mentorController.mentorAuthentication, mentorController.findMentorById);
 router.get("/", mentorController.findMentorByName);
 router.post("/add", mentorController.createNewMentor)
-router.patch("/:id", mentorController.updatementorById);
-router.delete("/delete/:id", mentorController.deleteMentorById)
 router.post("/login", mentorController.loginMentor);
+router.patch("/:id", mentorController.mentorAuthentication, mentorController.updatementorById);
+router.delete("/delete/:id", mentorController.mentorAuthentication, mentorController.deleteMentorById)
+
 
 module.exports = router
