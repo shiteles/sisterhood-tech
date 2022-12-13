2c73b3009b18f0765cec9f628eac2323ff0da0f6
Shirlene Teles <shiteles@gmail.com>
2022-12-13 17:49:59 -0300

    :safety_vest: Add authentication to routes

--- src/routes/menteeRoutes.js
+++ src/routes/menteeRoutes.js
@@ -2,7 +2,7 @@ const menteeController = require("../controllers/menteeController")
 const express = require("express")
 const router = express.Router();
 
-router.get("/all", menteeController.findAllMentee );
+router.get("/all", menteeController.findAllMentee);
 router.get("/:id", menteeController.menteeAuthentication, menteeController.findMenteeById);
 router.get("/", menteeController.findMenteeByName);
 router.post("/add", menteeController.createNewMentee)
